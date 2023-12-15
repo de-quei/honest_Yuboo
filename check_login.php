eddbc25f839426bcbaea907f75801db36015d7a4
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-12-15 11:28:40 +0900

    로그인 구현

---
+++ check_login.php
@@ -0,0 +1,29 @@
+<?php
+    include('./db_conn.php');
+
+    session_start();
+
+    $userid = $_POST['id'];
+    $userpw = $_POST['password'];
+
+    $query = "SELECT * FROM user WHERE id = '$userid' AND pw = '$userpw'";
+    $result = mysqli_query($conn, $query);
+    if (!$result) {
+        die('쿼리 실패: ' . mysqli_error($conn));
+    }
+    
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+
+    if($row != null){
+        $_SESSION['userid'] = $row['id'];
+        echo "<script>alert('안녕하세요, $userid 님')</script>";
+        echo "<script>location.replace('menu.html');</script>";
+        exit;
+    }
+
+    if($row == null){
+        echo "<script>alert('회원이 아닙니다.')</script>";
+        echo "<script>location.replace('signin.html')</script>";
+        exit;
+    }
+?>
